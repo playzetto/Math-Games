989bad5f113f2199ee3fca82ae35bd34fe02a734
Alexander Ugnyachev <yoscro@gmail.com>
2018-07-30 22:46:32 +0300

    Second excercise done

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/local/bin/node
-console.log('Welcome to the Brain Games!');
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!'); 
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}`);
