1529da4e8f08464cf06f7e0b8c17ac4949b253bf
Alexander Ugnyachev <yoscro@gmail.com>
2018-07-31 09:06:28 +0300

    Mistakes from first part fixed

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,4 @@
-#!/usr/local/bin/node
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!'); 
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}`);
