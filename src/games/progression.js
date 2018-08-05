8ab36cd371bffbcd474cdb0cefbfc8c5788a3e10
Alexander Ugnyachev <yoscro@gmail.com>
2018-08-05 11:25:44 +0300

    Stage 8 fixed

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,30 +2,31 @@ import playGame from '..';
 import getRandomNumber from '../utils';
 
 const rule = 'What number is missing in this progression?';
-const lengthOfProgression = 10;
-const startRange = [1, 20];
-const stepRange = [1, 5];
+
+
+const generateProgression = (progressionLength, firstNum, stepValue) => {
+  const progression = [];
+  progression.push(firstNum);
+  for (let i = 0; i < progressionLength; i += 1) {
+    progression.push(progression[i] + stepValue);
+  }
+  return progression;
+};
+
+const generateQuestion = (progressionArr) => {
+  const randomIndex = getRandomNumber(0, progressionArr.length - 1);
+  const correctAnswer = progressionArr[randomIndex].toString();
+  const copyArr = progressionArr; // Не копирует массив, а ссылается на один и тот же блок памяти
+  copyArr[randomIndex] = '..';
+  const question = copyArr.join(' ');
+  return [question, correctAnswer];
+};
 
 const genGameData = () => {
-  const generateProgression = () => {
-    const progression = [];
-    progression.push(getRandomNumber(startRange[0], startRange[1]));
-    const progressionStepRange = getRandomNumber(stepRange[0], stepRange[1]);
-    for (let i = 0; i < lengthOfProgression; i += 1) {
-      progression.push(progression[i] + progressionStepRange);
-    }
-    return progression;
-  };
-
-  const hideElement = (progressionArr) => {
-    const randomIndex = getRandomNumber(0, progressionArr.length - 1);
-    const correctAnswer = progressionArr[randomIndex].toString();
-    const copyArr = progressionArr;
-    copyArr[randomIndex] = '..';
-    const question = copyArr.join(' ');
-    return [question, correctAnswer];
-  };
-  return hideElement(generateProgression());
+  const progressionLen = 10;
+  const startNum = getRandomNumber(1, 20);
+  const stepVal = getRandomNumber(1, 5);
+  return generateQuestion(generateProgression(progressionLen, startNum, stepVal));
 };
 
 const startGame = () => playGame(genGameData, rule);
